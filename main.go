d1f016b2bb5712d3f0ca759aed1b463fabee6dfb
Arnold Okoth <kyaye.arnold@gmail.com>
2020-10-18 14:20:28 +0300

    Use the view type in main

--- main.go
+++ main.go
@@ -2,15 +2,68 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/arnoldokoth/lenslocked.com/views"
+	"github.com/gorilla/mux"
 )
 
-func main() {
-	http.HandleFunc("/", index)
-	http.ListenAndServe(":3000", nil)
+var (
+	homeView    *views.View
+	contactView *views.View
+	faqView     *views.View
+)
+
+func init() {
+	homeView = views.NewView("views/home.gohtml")
+	contactView = views.NewView("views/contact.gohtml")
+	faqView = views.NewView("views/faq.gohtml")
 }
 
-func index(w http.ResponseWriter, r *http.Request) {
+var error404Template = `
+<h3>Sorry. We could not find the page you're looking for.</h3>
+<p>Please contact us if you keep getting sent to an invalid page.</p>
+`
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/", home)
+	router.HandleFunc("/contact", contact)
+	router.HandleFunc("/faq", faq)
+	// router.NotFoundHandler = http.Handler(notFound)
+	log.Printf("Server Running On Port: %d", 3000)
+	http.ListenAndServe(":3000", router)
+}
+
+func must(err error) {
+	if err != nil {
+		log.Fatalln("ERROR:", err)
+	}
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome To LensLocked.com</h1>")
+	if err := homeView.Template.Execute(w, nil); err != nil {
+		panic(err)
+	}
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	if err := contactView.Template.Execute(w, nil); err != nil {
+		panic(err)
+	}
+}
+
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	if err := faqView.Template.Execute(w, nil); err != nil {
+		panic(err)
+	}
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, error404Template)
 }

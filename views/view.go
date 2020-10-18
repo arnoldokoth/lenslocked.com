91028d2a674d58610a32f3a1200d9fc8fb38163c
Arnold Okoth <kyaye.arnold@gmail.com>
2020-10-18 14:57:45 +0300

    Add layout parameter for NewView function

--- views/view.go
+++ views/view.go
@@ -3,8 +3,11 @@ package views
 import "html/template"
 
 // NewView ...
-func NewView(files ...string) *View {
-	files = append(files, "views/layouts/footer.gohtml")
+func NewView(layout string, files ...string) *View {
+	files = append(files,
+		"views/layouts/bootstrap.gohtml",
+		"views/layouts/footer.gohtml",
+	)
 	t, err := template.ParseFiles(files...)
 	if err != nil {
 		panic(err)
@@ -12,10 +15,12 @@ func NewView(files ...string) *View {
 
 	return &View{
 		Template: t,
+		Layout:   layout,
 	}
 }
 
 // View ...
 type View struct {
 	Template *template.Template
+	Layout   string
 }

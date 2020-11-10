1875a2cef950288a0790438443fb4ecf6c4e559e
Arnold Okoth <kyaye.arnold@gmail.com>
2020-11-10 14:36:25 +0300

    Move static views and add changes to main

--- main.go
+++ main.go
@@ -1,37 +1,28 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 
-	"github.com/arnoldokoth/lenslocked.com/views"
+	"github.com/arnoldokoth/lenslocked.com/controllers"
 	"github.com/gorilla/mux"
 )
 
-var (
-	homeView    *views.View
-	contactView *views.View
-	faqView     *views.View
-)
-
-func init() {
-	homeView = views.NewView("bootstrap", "views/home.gohtml")
-	contactView = views.NewView("bootstrap", "views/contact.gohtml")
-	faqView = views.NewView("bootstrap", "views/faq.gohtml")
-}
-
-var error404Template = `
-<h3>Sorry. We could not find the page you're looking for.</h3>
-<p>Please contact us if you keep getting sent to an invalid page.</p>
-`
-
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/", home)
-	router.HandleFunc("/contact", contact)
-	router.HandleFunc("/faq", faq)
-	// router.NotFoundHandler = http.Handler(notFound)
+
+	staticController := controllers.NewStatic()
+	usersController := controllers.NewUsers()
+
+	// Static Routes
+	router.Handle("/", staticController.HomeView).Methods("GET")
+	router.Handle("/contact", staticController.ContactView).Methods("GET")
+	router.Handle("/faq", staticController.FAQView).Methods("GET")
+
+	// User Routes
+	router.HandleFunc("/signup", usersController.New).Methods("GET")
+	router.HandleFunc("/signup", usersController.Create).Methods("POST")
+
 	log.Printf("Server Running On Port: %d", 3000)
 	http.ListenAndServe(":3000", router)
 }
@@ -40,30 +31,4 @@ func must(err error) {
 	if err != nil {
 		log.Fatalln("ERROR:", err)
 	}
-}
-
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	if err := homeView.Template.ExecuteTemplate(w, homeView.Layout, nil); err != nil {
-		panic(err)
-	}
-}
-
-func contact(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	if err := contactView.Template.ExecuteTemplate(w, contactView.Layout, nil); err != nil {
-		panic(err)
-	}
-}
-
-func faq(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	if err := faqView.Template.ExecuteTemplate(w, faqView.Layout, nil); err != nil {
-		panic(err)
-	}
-}
-
-func notFound(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNotFound)
-	fmt.Fprint(w, error404Template)
 }
--- views/view.go
+++ views/view.go
@@ -1,13 +1,23 @@
 package views
 
-import "html/template"
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+)
+
+var (
+	templateDir        = "views/"
+	layoutDir   string = "views/layouts/"
+	templateExt string = ".gohtml"
+)
 
 // NewView ...
 func NewView(layout string, files ...string) *View {
-	files = append(files,
-		"views/layouts/bootstrap.gohtml",
-		"views/layouts/footer.gohtml",
-	)
+	addTemplatePath(files)
+	addTemplateExt(files)
+	files = append(files, layoutFiles()...)
 	t, err := template.ParseFiles(files...)
 	if err != nil {
 		panic(err)
@@ -23,4 +33,40 @@ func NewView(layout string, files ...string) *View {
 type View struct {
 	Template *template.Template
 	Layout   string
+}
+
+func (v *View) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	v.Render(w, nil)
+}
+
+// Render ...
+func (v *View) Render(w http.ResponseWriter, data interface{}) error {
+	w.Header().Set("Content-Type", "text/html")
+	return v.Template.ExecuteTemplate(w, v.Layout, data)
+}
+
+// layoutFiles returns the list of files in
+// the views/layouts directory
+func layoutFiles() []string {
+	files, err := filepath.Glob(layoutDir + "*" + templateExt)
+	if err != nil {
+		log.Fatalln("layoutFiles() ERROR:", err)
+	}
+
+	return files
+}
+
+// addTemplatePath takes in a slice of strings
+// representing file paths for templates and
+// prepends the templateDir to each string
+func addTemplatePath(files []string) {
+	for idx, file := range files {
+		files[idx] = templateDir + file
+	}
+}
+
+func addTemplateExt(files []string) {
+	for idx, file := range files {
+		files[idx] = file + templateExt
+	}
 }

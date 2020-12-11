77ea5d472d81823d73defdf8b7ac8c8e8846be77
Arnold Okoth <kyaye.arnold@gmail.com>
2020-12-11 16:34:32 +0300

    Add middleware package

---
+++ middleware/require_user.go
@@ -0,0 +1,39 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/arnoldokoth/lenslocked.com/context"
+	"github.com/arnoldokoth/lenslocked.com/models"
+)
+
+// RequireUser ...
+type RequireUser struct {
+	models.UserService
+}
+
+func (mw *RequireUser) Apply(next http.Handler) http.HandlerFunc {
+	return mw.ApplyFn(next.ServeHTTP)
+}
+
+// ApplyFn ...
+func (mw *RequireUser) ApplyFn(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cookie, err := r.Cookie("remember_token")
+		if err != nil {
+			http.Redirect(w, r, "/login", http.StatusFound)
+			return
+		}
+		user, err := mw.UserService.ByRemember(cookie.Value)
+		if err != nil {
+			http.Redirect(w, r, "/login", http.StatusFound)
+			return
+		}
+
+		ctx := r.Context()
+		ctx = context.WithUser(ctx, user)
+		r = r.WithContext(ctx)
+
+		next(w, r)
+	})
+}
